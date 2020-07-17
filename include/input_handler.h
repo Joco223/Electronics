4d2621bf9a8e6119893c13695ba128215954c9de
Joco223 <jovanmihajlovic22@gmail.com>
2020-07-18 00:31:07 +0200

    Weird error

---
+++ include/input_handler.h
@@ -0,0 +1,3 @@
+#include <vector>
+
+std::vector<bool> pressed_keys = {false, false, false, false};
--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+#include "input_handler.h"
 
 int main(int argc, char** argv) {
-    printf("Hello World!\n");
 
-    return 0;
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Electronics");
+	window.setFramerateLimit(60);
+	sf::View camera = window.getDefaultView();
+
+	while (window.isOpen()) {
+		sf::Event Event;
+		while (window.pollEvent(Event)) {
+			if (Event.type == sf::Event::Closed) {
+				window.close();
+			}else if (Event.type == sf::Event::KeyPressed) {
+
+			}else if (Event.type == sf::Event::KeyReleased) {
+
+			}
+		}
+
+		window.setView(camera);
+		window.clear();
+
+		//All the drawing goes here
+
+		window.display();
+	}
+
+	return 0;
 }
